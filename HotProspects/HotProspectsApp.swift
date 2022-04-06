f5356f5ef1bd4ea9533fa4f1b325f10c6883cb03
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-04-06 14:51:34 -0600

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Mitch Andrade on 4/6/22.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
