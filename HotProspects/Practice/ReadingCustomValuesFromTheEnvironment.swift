0db571743df5b1497823c829996f2b31473e5db9
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-04-06 15:16:41 -0600

    first commit

---
+++ HotProspects/Practice/ReadingCustomValuesFromTheEnvironment.swift
@@ -0,0 +1,20 @@
+//
+//  ReadingCustomValuesFromTheEnvironment.swift
+//  HotProspects
+//
+//  Created by Mitch Andrade on 4/6/22.
+//
+
+import SwiftUI
+
+struct ReadingCustomValuesFromTheEnvironment: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ReadingCustomValuesFromTheEnvironment_Previews: PreviewProvider {
+    static var previews: some View {
+        ReadingCustomValuesFromTheEnvironment()
+    }
+}
