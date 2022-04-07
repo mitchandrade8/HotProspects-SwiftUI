bc20263ac0f3025d46a57690665bb025ce6a6e4a
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-04-07 15:03:45 -0600

    fourth commit

---
+++ HotProspects/Practice/AddingSwiftPackageDependenciesInXcode.swift
@@ -0,0 +1,20 @@
+//
+//  AddingSwiftPackageDependenciesInXcode.swift
+//  HotProspects
+//
+//  Created by Mitch Andrade on 4/7/22.
+//
+
+import SwiftUI
+
+struct AddingSwiftPackageDependenciesInXcode: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddingSwiftPackageDependenciesInXcode_Previews: PreviewProvider {
+    static var previews: some View {
+        AddingSwiftPackageDependenciesInXcode()
+    }
+}
---
+++ HotProspects/Prospect.swift
@@ -0,0 +1,8 @@
+//
+//  Prospect.swift
+//  HotProspects
+//
+//  Created by Mitch Andrade on 4/7/22.
+//
+
+import Foundation
