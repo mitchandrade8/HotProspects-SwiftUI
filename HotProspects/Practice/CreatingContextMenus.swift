bec916d1c6a68499fed2fdd211e1e7a2c36dbf70
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-04-06 18:28:38 -0600

    third commit

---
+++ HotProspects/Practice/CreatingContextMenus.swift
@@ -0,0 +1,20 @@
+//
+//  CreatingContextMenus.swift
+//  HotProspects
+//
+//  Created by Mitch Andrade on 4/6/22.
+//
+
+import SwiftUI
+
+struct CreatingContextMenus: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreatingContextMenus_Previews: PreviewProvider {
+    static var previews: some View {
+        CreatingContextMenus()
+    }
+}
---
+++ HotProspects/Practice/SchedulingLocalNotifications.swift
@@ -0,0 +1,20 @@
+//
+//  SchedulingLocalNotifications.swift
+//  HotProspects
+//
+//  Created by Mitch Andrade on 4/6/22.
+//
+
+import SwiftUI
+
+struct SchedulingLocalNotifications: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SchedulingLocalNotifications_Previews: PreviewProvider {
+    static var previews: some View {
+        SchedulingLocalNotifications()
+    }
+}
